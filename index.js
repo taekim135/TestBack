b29b02c12e0b05cbfb2a89a22aadc84b47d20ff0
taekim135 <tkim363@uwo.ca>
2025-12-26 11:10:03 +0900

    Initial Push

---
+++ index.js
@@ -0,0 +1,105 @@
+// Notes Backend Server
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+
+const generateID = () =>{
+  //Spread syntax "expands" an array into its elements, while rest syntax collects multiple elements and "condenses" them into a single element
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id))) 
+    : 0
+
+  const note = request.body
+  return String(maxId + 1)
+}
+
+
+app.get("/", (request,response) =>{
+  response.send("<h1>Hello World!</h1>")
+})
+
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+
+app.get("/api/notes/:id", (request,response) => {
+  const id = request.params.id
+  const note = notes.find(note => note.id === id)
+
+  if (note){
+    response.json(note)
+  }else{
+    response.status(404).end()
+  }
+})
+
+
+app.delete("/api/notes/:id", (request,response) => {
+  const id = request.params.id
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body){
+    return response.status(400).json({
+      error: "content empty"
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateID()
+  }
+  
+
+  notes = notes.concat(note)
+  response.json(note)
+})
+
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Backened Server (Notes data) running on port http://localhost:${PORT}`)
+})
