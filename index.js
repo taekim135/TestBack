dcc0d621ac8757db717f5b2eb82db8b32b6785eb
taekim135 <tkim363@uwo.ca>
2025-12-28 00:50:52 +0900

    cors removed

--- index.js
+++ index.js
@@ -2,9 +2,7 @@
 
 const express = require('express')
 const app = express()
-const cors = require('cors')
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 //serve front end files
