b67043c8a548c7bab782c76b423f55c791ca1cfb
taekim135 <tkim363@uwo.ca>
2025-12-29 13:50:36 +0900

    logs format added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 const app = express()
 const Note = require('./models/note')
 
-// cors not needed as same origins now (port,domain,protocol)
+//cors not needed as same origins now (port,domain,protocol)
 //const cors = require('cors')
 //app.use(cors())
 
@@ -34,11 +34,21 @@ let notes = [
   }
 ]
 
+const logs = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---------')
+  next()
+}
+
+app.use(logs)
+
 
 // not needed as we are using dist folder
-// app.get("/", (request,response) =>{
-//   response.send("<h1>Hello World!</h1>")
-// })
+app.get("/", (request,response) =>{
+  response.send("<h1>Hello World!</h1>")
+})
 
 
 // fetching notes straight from mongoDB
@@ -50,14 +60,9 @@ app.get('/api/notes', (request, response) => {
 
 
 app.get("/api/notes/:id", (request,response) => {
-  const id = request.params.id
-  const note = notes.find(note => note.id === id)
-
-  if (note){
+  Note.findById(request.params.id).then(note=>{
     response.json(note)
-  }else{
-    response.status(404).end()
-  }
+  })
 })
 
 
@@ -76,14 +81,14 @@ app.post('/api/notes', (request, response) => {
     return response.status(400).json({ error: "content empty" })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false
-  }
+  })
   
-
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then(savedNote =>{
+    response.json(savedNote)
+  })
 })
 
 
