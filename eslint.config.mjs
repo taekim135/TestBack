d62e495ea3a400578eede7a6e964580a9eeba0bc
taekim135 <tkim363@uwo.ca>
2026-01-06 11:11:53 +0900

    es lint added

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin'
+
+
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
+      ecmaVersion: 'latest',
+    },
+    plugins: { 
+      '@stylistic/js': stylisticJs,
+    },
+    rules: { 
+      '@stylistic/js/indent': ['error', 4],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'double'],
+      '@stylistic/js/semi': ['error', 'never'],
+    }, 
+  },
+]
